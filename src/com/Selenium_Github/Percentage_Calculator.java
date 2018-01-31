035adbb022a14b26dde788f47b74568bbc9f5500
hp <hp@hp-PC>
2018-01-31 09:14:04 +0530

    First Commit

---
+++ src/com/Selenium_Github/Percentage_Calculator.java
@@ -0,0 +1,24 @@
+package com.Selenium_Github;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Percentage_Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\DUCAT\\Seleniam Automation\\Gmail Login\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("http://www.calculator.net");
+		driver.findElement(By.linkText("Math Calculators")).click();
+		driver.findElement(By.linkText("Percentage Calculator")).click();
+		driver.findElement(By.cssSelector("#cpar1")).sendKeys("50");
+		driver.findElement(By.cssSelector("#cpar2")).sendKeys("5");
+		driver.findElement(By.cssSelector("#content > table:nth-child(4) > tbody > tr:nth-child(2) > td > input[type='image']:nth-child(2)")).click();
+
+
+	}
+
+}
